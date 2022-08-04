1d69bd576a0fdf41409df09c399859d563b58edb
leebongkyu <leebongkyu@hanmail.net>
2022-08-04 14:28:23 +0900

    프런트 로그인 구현

--- app/app.js
+++ app/app.js
@@ -15,8 +15,10 @@
 "use strict";
 
 //모듈
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
 
 //라우팅
 const home = require("./src/routes/home");
@@ -26,6 +28,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/src/public/'))
-app.use("/", home); //use -> 미들웨어를 등록해 주는 메소드
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use("/", home); //use -> 미들웨어를 등록해 주는 메소드
 module.exports = app;
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,13 +11,22 @@ function login() {
         psword: psword.value,
     };
 
-    console.log(req);
-    console.log(JSON.stringify(req));
     fetch("/login", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
+        body: JSON.stringify(req),
     })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error(new Error("로그인 중 에러 발생"));
+        });
 }

b0c5763a56f8e3cb20a8274abc0f293dbac4681a
leebongkyu <leebongkyu@hanmail.net>
2022-08-06 16:41:19 +0900

    회원가입 라우팅 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -25,6 +25,12 @@ const process = {
         //console.log(response);
         return res.json(response);
     },
+
+    register: (req, res) => {
+        const user = new User(req.body);
+        const response = user.register()
+        return res.json(response);
+    }
 }
 
 module.exports = {

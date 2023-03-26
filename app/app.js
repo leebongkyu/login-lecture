813d126d7d674b4e25eba0edb29da6dc242bd8df
leebongkyu <leebongkyu@hanmail.net>
2023-03-26 16:48:55 +0900

    자바스트립트단 만들기

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // user -> 미들웨어 등록
 

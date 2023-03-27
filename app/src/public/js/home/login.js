6ba41b80527682f4fce7d280a8d30269f00a6c73
leebongkyu <leebongkyu@hanmail.net>
2023-03-28 08:37:24 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,5 +8,14 @@ $(function() {
         };
 
         console.log(req);
+        console.log(JSON.stringify(req))
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req),
+        });
     });
 });
